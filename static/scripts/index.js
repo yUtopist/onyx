9fc089b047ff8a8464df100dab1679abc4a34906
Alek Groove <stukalov.nz@gmail.com>
2023-07-09 12:59:21 +1200

    Hero Heading Effect Finished

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -10,29 +10,28 @@ heroList.forEach(heading => {
 
   content.forEach((char, index) => {
     const handleEnter = (index) => {
-      heading.children[index].dataset.state = 'hover'
-      if (heading.children[index - 1]) {
-        heading.children[index - 1].dataset.state = 'sided'
-      }
-      if (heading.children[index + 1]) {
-        heading.children[index + 1].dataset.state = 'sided'
-      }
+      const middle = heading.children[index]
+      const sideLeft = heading.children[index - 1]
+      const sideRight = heading.children[index + 1]
+      const grandLeft = heading.children[index - 2]
+      const grandRight = heading.children[index + 2]
+
+      middle.dataset.state = 'middle'
+      if (sideLeft) sideLeft.dataset.state = 'side'
+      if (sideRight) sideRight.dataset.state = 'side'
+      if (grandLeft) grandLeft.dataset.state = 'grand'
+      if (grandRight) grandRight.dataset.state = 'grand'
     }
     
-    const handleLeave = (index) => {
-      heading.children[index].dataset.state = 'idle'
-      if (heading.children[index - 1]) {
-        heading.children[index - 1].dataset.state = 'idle'
-      }
-      if (heading.children[index + 1]) {
-        heading.children[index + 1].dataset.state = 'idle'
-      }
+    const handleLeave = () => {
+      const characters = Array.from(heading.children)
+      characters.forEach(char => char.dataset.state = 'idle')
     }
 
     const span = document.createElement('span')
-    span.textContent = char
+    span.textContent = char === ' ' ? '\u00A0' : char
     span.onmouseenter = () => handleEnter(index)
-    span.onmouseleave = () => handleLeave(index)
+    span.onmouseleave = () => handleLeave()
     heading.appendChild(span)
   })
 })
